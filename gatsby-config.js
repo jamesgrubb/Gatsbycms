78358d29abfc1dbe8ea570e7167f966932e761a4
jamesgrubb <jamesgrubb@mac.com>
2019-06-19 19:24:17 +0100

    Squashed commit of the following:
    commit 5510028d255bcdcaa38ed323ad039b6047eaf0e3
    Author: jamesgrubb <jamesgrubb@mac.com>
    Date:   Wed Jun 19 19:21:08 2019 +0100
    
        Adding Markdown
    
    commit 09a8e625989690a7f52042a0c5e37ebe236d7197
    Author: jamesgrubb <jamesgrubb@mac.com>
    Date:   Wed Jun 19 19:13:14 2019 +0100
    
        gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,19 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `markdown-pages`,
+      path: `${__dirname}/blog/`
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/blog/`,
+      name: `markdown-pages`,
+    },
+  },
+  `gatsby-transformer-remark`,
+    ],
 }
